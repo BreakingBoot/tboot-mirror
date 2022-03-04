49b511acd9bb1d4a84888302ea3ad98e3d76a215
Pawel Randzio <pawel.randzio@intel.com>
2022-03-04 11:14:21 +0100

    Version v1.10.5

--- tboot/Config.mk
+++ tboot/Config.mk
@@ -6,8 +6,8 @@
 #
 # tboot-specific build settings
 #
-RELEASEVER  := "1.10.4"
-RELEASETIME := "2022-02-24 12:00 +0100"
+RELEASEVER  := "1.10.5"
+RELEASETIME := "2022-03-04 12:00 +0100"
 ROOTDIR ?= $(CURDIR)/..
 
 # tboot needs too many customized compiler settings to use system CFLAGS,
