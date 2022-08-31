c11149bd6020320bb62ad140cdb34c21c28e4cb9
Pawel Randzio <pawel.randzio@intel.com>
2022-08-31 15:20:39 +0200

    Add deprecation warning for "agile" extend policy option
    As has been requested about 8 months ago by Alex Eydelberg,
    the "agile" option for extending policies will be deprecated to
    limit the use of TPM hashing algorithms.
    
    For now I'm only leaving a warning about soon deprecation of this
    option. Change definitely removing this option and making it go
    into default setting will appear around October this year.
    
    By chance also adding VS Code files into .hgignore for my own
    petty convenience...

--- tboot/common/cmdline.c
+++ tboot/common/cmdline.c
@@ -515,6 +515,9 @@ void get_tboot_extpol(void)
     }
 
     if ( tb_strcmp(extpol, "agile") == 0 ) {
+        printk(TBOOT_WARN"Warning: \"agile\" selected in extpol, "
+                         "this option is set to be deprecated\n"
+                         "(see tboot-devel mailing list archive)\n");
         tpm->extpol = TB_EXTPOL_AGILE;
         tpm->cur_alg = TB_HALG_SHA256;
     } else if ( tb_strcmp(extpol, "embedded") == 0 ) {
